01410216b86b4fa06844a0f81487946030380e65
ArKloK <ccamachobanez@gmail.com>
2024-03-04 14:45:11 +0100

    New invoice document format

---
+++ ssg_document_format/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "SSG Document Format",
+    "summary": """Formatos de documentos para SSG""",
+    "version": "16.1.0.0",
+    "description": """Formatos de documentos para SSG""",
+    "author": "Carlos Camacho",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "stock",
+    ],
+    "data": [
+        "views/invoice/invoice_document.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
